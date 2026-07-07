b43ae5a3d0289a6fd5970829b71dd546d9d511da
thebankbugs <thebankbugs@gmail.com>
2026-07-07 23:16:38 +1000

    Update splash.js

--- js/splash.js
+++ js/splash.js
@@ -1,1 +1,55 @@
+const messages = [
 
+    "Initializing BankBugs|FX...",
+
+    "Loading Trading systems...",
+
+    "Preparing Starter Dashboard...",
+
+    "Loading Elite Experience...",
+
+    "Syncing Platform...",
+
+    "Ready..."
+
+];
+
+const loadingText = document.getElementById("loading-text");
+
+let index = 0;
+
+const interval = setInterval(() => {
+
+    index++;
+
+    if(index < messages.length){
+
+        loadingText.textContent = messages[index];
+
+    }
+
+},500);
+
+setTimeout(()=>{
+
+    clearInterval(interval);
+
+    document.body.style.opacity="0";
+
+    document.body.style.transition="opacity .6s";
+
+    setTimeout(()=>{
+
+        window.location.href="homepage/home.html";
+
+    },600);
+
+},3500);
+
+setTimeout(() => {
+  document.body.classList.add("fade-out");
+
+  setTimeout(() => {
+    window.location.href = "homepage/home.html";
+  }, 800);
+}, 3000);
